a5644bddac4c0312738047989813acfbb148bd9e
unknown <paulo.ricardo.marolla@hotmail.com>
2021-03-17 20:54:37 -0300

    estilizacoes dos componentes da dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { GlobalStyle } from './styled/global';
 import { Header } from './components/Header/index'
+import { Dashboard } from './components/Dashboard';
 
 function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
