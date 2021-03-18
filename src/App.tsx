dc3f5ee37f617997e1b24676efb99625fb206dc7
unknown <paulo.ricardo.marolla@hotmail.com>
2021-03-18 18:43:56 -0300

    instalacao do miragejs para API fake

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,26 @@ import React from 'react';
 import { GlobalStyle } from './styled/global';
 import { Header } from './components/Header/index'
 import { Dashboard } from './components/Dashboard';
+import { createServer } from 'miragejs'
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 function App() {
   return (
