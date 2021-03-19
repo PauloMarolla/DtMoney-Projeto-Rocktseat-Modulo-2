0ebb39d89bb8ec229256df5604204e7282607a4c
unknown <paulo.ricardo.marolla@hotmail.com>
2021-03-19 20:15:04 -0300

    criando db fake com miragejs

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,8 @@ import Modal from 'react-modal';
 import { GlobalStyle } from './styled/global';
 import { Header } from './components/Header/index'
 import { Dashboard } from './components/Dashboard';
-import { createServer } from 'miragejs'
 import { NewTransactionsMOdal } from './components/NewTransactionsModal';
 
-createServer({
-  routes() {
-    this.namespace = 'api';
-
-    this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createAt: new Date()
-        }
-      ]
-    })
-  }
-})
 
 Modal.setAppElement('#root')
 
